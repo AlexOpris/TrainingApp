60d5b637482b901e280b34311ba5422c44150267
Alex-Florin Opris <alex.opris@smedix.com>
2024-05-24 15:01:19 +0300

    end of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,4 @@
-using System.Text;
 using API;
-using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +11,8 @@ builder.Services.AddIdentityService(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
